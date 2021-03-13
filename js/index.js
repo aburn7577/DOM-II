a0af85a284587a5adec1814701691911595497b1
Ashley Burns <ashley@ashleys-mini.home>
2021-03-13 14:03:37 -0500

    2 events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const top = document.querySelector('.main-navigation')
+const navBar = document.querySelectorAll('.nav-link') //get element
+
+navBar.forEach(link => {
+    link.addEventListener('mouseover', event => {
+        event.target.style.color = "purple"
+    })
+    link.addEventListener('mouseout', event => {
+        event.target.style.color = "black"
+    })
+})
