efdbfab9b954455bf715bca6a25e90e9c0ad85c7
Ashley Burns <ashley@ashleys-mini.home>
2021-03-13 17:10:35 -0500

    5 listeners done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,42 @@
 // Your code goes here
-const top = document.querySelector('.main-navigation')
-const navBar = document.querySelectorAll('.nav-link') //get element
+const top = document.querySelector('header');
+const logo = document.querySelector(".logo-heading");
+const navBar = document.querySelectorAll('.nav-link');
+const pic = document.querySelectorAll('img')
 
+// onload------------------------11111
+window.onload = function(){
+    top.style.backgroundColor = 'teal'
+}
+
+        // nav bar links
+// mouseover--------------------22222
+// mouseout---------------------33333
 navBar.forEach(link => {
     link.addEventListener('mouseover', event => {
-        event.target.style.color = "purple"
+        event.target.style.color = "limegreen";
     })
     link.addEventListener('mouseout', event => {
-        event.target.style.color = "black"
+        event.target.style.color = "black";
     })
+})
+
+
+// header bar click to change body
+// click-------------------------44444
+top.addEventListener('click', () => {
+    const color = top.style.backgroundColor;
+     if (color === 'maroon'){
+         top.style.backgroundColor = 'pink';
+     }
+     else {
+         top.style.backgroundColor = 'maroon';
+     }
+});
+
+// keydown------------------------55555
+document.addEventListener('keydown', event =>{
+    if (event.key === "Escape"){
+        alert('Where are you going?!!!')
+    }
 })
